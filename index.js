141ad2e69dac6be021bd49a6e780bef46893554f
Tevin Langat <67066313+Tevinlangat@users.noreply.github.com>
2023-09-08 18:25:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-const express = require('express')
+import express, { json } from 'express'
+import cors from 'cors'
+
+// configurations
+import 'dotenv/config'
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const PORT = process.env.PORT || 9000
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+// Api endpoints
+app.get('/api',(req,res)=>{
+    const { slack_name, track } = req.query;
+
+    if (!slack_name && !track){
+        res.status(404).json({"error":"parameters missing"})
+    }
+   
+    const date = new Date()
+    const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+    const day = days[date.getDay()]
+
+    res.status(200).json({
+        "slack_name":slack_name,
+        "current_day":day,
+        "utc_time":date,
+        "track":track,
+        "github_file_url": "https://github.com/Tevinlangat/HNG-tasks/blob/main/index.js",
+        "github_repo_url": "https://github.com/Tevinlangat/HNG-tasks",
+        "status_code":200
+
+    })
 })
-app.listen(process.env.PORT || 3000)
+
+// listener
+app.listen(PORT,()=>{
+    console.log(`server is open at port ${PORT}`)
+})
+
+
